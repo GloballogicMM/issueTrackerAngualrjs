427e4e50b290dde9e90c0a8e157a4cb72ab0b58c
Max <maksr51314@gmail.com>
2013-07-22 12:30:22 +0300

    Create new storage

--- js/controllers/todoCtrl.js
+++ js/controllers/todoCtrl.js
@@ -1,26 +1,25 @@
 todo.controller('TodoCtrl', function TodoCtrl($scope, todoStorage) {
-	var todos = $scope.todos = todoStorage.getTitle();
+	var todos = $scope.todos = todoStorage.get();
 
     //if smth change in list then upload storage
 	$scope.$watch('todos', function () {
-		todoStorage.putTitle(todos);
+		todoStorage.put(todos);
 	}, true);
 
 	$scope.addTodo = function () {
-		var newTodo = $scope.newTodo.trim();
+		var newTodo = $scope.newTodo;
             if (!newTodo.length) {
 			return;
 		}
-
 		todos.push({
 			title: newTodo,
+            desc: $scope.newDesc,
 			completed: false
 		});
-
 		$scope.newTodo = '';
 	};
 
-	$scope.editTodo = function (todo) {
+	$scope.editTodo = function () {
         $scope.editing = true;
 	};
 
--- js/services/todoStorage.js
+++ js/services/todoStorage.js
@@ -1,18 +1,12 @@
-todo.factory('todoStorage', function () { 
-	var STORAGE_ID = 'todos-angularjs-title',
-        STORAGE_DESC = 'todos-angularjs-desc';
+todo.factory('todoStorage', function () {
+	var STORAGE_ID = 'todos-angularjs-title';
 	return {
-		getTitle: function () {
+		get: function () {
 			return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
 		},
-		putTitle: function (todos) {
+		put: function (todos) {
 			localStorage.setItem(STORAGE_ID, JSON.stringify(todos));
-		},
-        getDesc: function () {
-            return JSON.parse(localStorage.getDesc(STORAGE_DESC) || '[]');
-        },
-        putDesc: function (desc) {
-            localStorage.setItem(STORAGE_DESC, JSON.stringify(desc));
         }
+
 	};
 });
