55ee5099379828c0f122d57c7e52b698fbd8884c
mariia.verbenko <mariia.verbenko@globallogic.com>
2013-07-22 12:51:34 +0300

    Refactor
    Adding navigation bar with search

--- js/services/todoStorage.js
+++ js/services/todoStorage.js
@@ -1,11 +1,23 @@
 todo.factory('todoStorage', function () {
-	var STORAGE_ID = 'todos-angularjs-title';
+	var STORAGE_ID = 'todos-angularjs-title',
+        statuses = ['Defined', 'In progress', 'Completed', 'Blocked'];
+
+    function History(title, desc, date, estimate, status) {
+        this.title = title;
+        this.desc = desc;
+        this.date = date;
+        this.tasks = [];
+        this.estimate = estimate;
+        this.status = statuses[status];
+        return this;
+    };
+
 	return {
 		get: function () {
 			return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
 		},
-		put: function (todos) {
-			localStorage.setItem(STORAGE_ID, JSON.stringify(todos));
+		put: function (title, desc, date, estimate, status) {
+			localStorage.setItem(STORAGE_ID, JSON.stringify(new History(title, desc, date, estimate, status)));
         }
 
 	};
