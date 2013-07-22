02e42b5623f57ffcaca0435d4f8798b5ab40525a
mariia.verbenko <mariia.verbenko@globallogic.com>
2013-07-22 14:04:12 +0300

    Defined routing

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 var todo = angular.module('todo', []);
+todo.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/new', {templateUrl: 'partials/new.html', controller: 'todoCtrl'});
+    $routeProvider.otherwise({redirectTo: '/'});
+}]);
+
+todo.directive('todo', function () {
+    return {
+        restrict: 'E',
+        transclude: true,
+        templateUrl: 'partials/new.html'
+    }
+});
