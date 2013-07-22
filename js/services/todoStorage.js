8271f3b27fa2d655074601cfc435d5e9767df337
Max <maksr51314@gmail.com>
2013-07-22 11:32:16 +0300

    connect

--- js/services/todoStorage.js
+++ js/services/todoStorage.js
@@ -1,13 +1,18 @@
 todo.factory('todoStorage', function () {
-	var STORAGE_ID = 'todos-angularjs-title';
-
+	var STORAGE_ID = 'todos-angularjs-title',
+        STORAGE_DESC = 'todos-angularjs-desc';
 	return {
 		getTitle: function () {
 			return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
 		},
-
 		putTitle: function (todos) {
 			localStorage.setItem(STORAGE_ID, JSON.stringify(todos));
-		}
+		},
+        getDesc: function () {
+            return JSON.parse(localStorage.getDesc(STORAGE_DESC) || '[]');
+        },
+        putDesc: function (desc) {
+            localStorage.setItem(STORAGE_DESC, JSON.stringify(desc));
+        }
 	};
 });
