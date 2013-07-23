03d9d0d3a2384a9d52aa8591a27eebba065f8758
mariia.verbenko <mariia.verbenko@globallogic.com>
2013-07-23 14:22:31 +0300

    Defined weeks in object

--- js/controllers/asideCtrl.js
+++ js/controllers/asideCtrl.js
@@ -10,33 +10,69 @@ todo.controller("asideCtrl", function asideCtrl($scope) {
         {name: 'Sergei Sergeev', status: 1, text: 'b'},
         {name: 'Petr Petrovich', status: 2, text: 'aa'}
     ];
-
-    $scope.mockhistory = [
-        {
+    $scope.mockhistory = [];
+    addHistory({
             title: 'Search1',
             status: 0,
             user: [0],
+            date: new Date(2013, 6, 15),
             estimate: 4
-        },
-        {
+        });
+    addHistory({
             title: 'Search2',
             status: 1,
             user: [0],
+            date: new Date(2013, 6, 17),
             estimate: 4
-        },
-        {
+        });
+    addHistory({
             title: 'Search3',
             status: 2,
             user: [0],
+            date: new Date(2013, 6, 22),
             estimate: 4
-        },
-        {
+        });
+    addHistory({
             title: 'Item1',
             status: 3,
             user: [0, 1],
+            date: new Date(2013, 6, 25),
             estimate: 4
+        });
+
+    function addHistory(history) {
+        history.week = history.date.getWeek();
+        $scope.mockhistory.push(history);
+        $scope.mockhistory.sort(function(a,b) { return a.week- b.week;});
+        $scope.startWeek = $scope.mockhistory[0].week;
+        $scope.endWeek = $scope.mockhistory[$scope.mockhistory.length-1].week;
+    }
+
+    $scope.weeks = getWeeksNames($scope.startWeek, $scope.endWeek);
+    console.log($scope.weeks);
+
+    function getWeeksNames(startWeek, endWeek) {
+        var weeksNames = [],
+            weekName,
+            monday,
+            sunday;
+
+        for (var i = startWeek; i <= endWeek; i++) {
+            monday = Date.today().setWeek(i);
+            sunday = new Date(monday).moveToDayOfWeek(0);
+
+            weekName = monday.toString("MMMM, dd") + ' - ';
+
+            if (monday.toString("M") === sunday.toString("M")) {
+                weekName += sunday.toString("dd");
+            } else {
+                weekName += sunday.toString("MMMM, dd");
+            }
+
+            weeksNames.push(weekName);
         }
-    ];
+        return weeksNames;
+    }
 
     $scope.searchByStatus = function(status) {
         if ($scope.search.statuses.indexOf(status)<0) {
