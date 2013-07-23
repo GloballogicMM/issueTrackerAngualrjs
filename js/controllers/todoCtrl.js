bb906c0a63c5eff68979fc86513fca2d3aca35ca
Max <maksr51314@gmail.com>
2013-07-23 16:13:14 +0300

    create new Controller

--- js/controllers/todoCtrl.js
+++ js/controllers/todoCtrl.js
@@ -1,45 +1,45 @@
 todo.controller('todoCtrl', function TodoCtrl($scope, todoStorage) {
-	var todos = $scope.todos = todoStorage.get(),
+    var todos = $scope.todos = todoStorage.get(),
         oldTitle, oldDesc, oldDate;
 
     $scope.editing = false;
 
     //if smth change in list then upload storage
-	$scope.$watch('todos', function () {
-		todoStorage.put(todos);
-	}, true);
+    $scope.$watch('todos', function () {
+        todoStorage.put(todos);
+    }, true);
 
-	$scope.addTodo = function () {
-		var newTodo = $scope.newTodo;
-            if (!newTodo.length) {
-			return;
-		}
-		$scope.todos.push({
-			title: newTodo,
+    $scope.addTodo = function () {
+        var newTodo = $scope.newTodo;
+        if (!newTodo.length) {
+            return;
+        }
+        $scope.todos.push({
+            title: newTodo,
             desc: $scope.newDesc,
             date: $scope.newDate,
-			completed: false
-		});
-		$scope.newTodo = '';
+            completed: false
+        });
+        $scope.newTodo = '';
         $scope.newDesc = '';
         $scope.newDate = '';
-	};
+    };
 
-	$scope.editTodo = function (todo) {
+    $scope.editTodo = function (todo) {
         $scope.editing = true;
         todo.editing = true;
         oldTitle = todo.title;
         oldDesc = todo.desc;
         oldDate = todo.date;
-	};
+    };
 
-	$scope.doneEditing = function (todo) {
-		if (!todo.title) {
-			$scope.removeTodo(todo);
-		}
+    $scope.doneEditing = function (todo) {
+        if (!todo.title) {
+            $scope.removeTodo(todo);
+        }
         todo.editing = false;
         $scope.editing = false;
-	};
+    };
 
     $scope.cancelEdit = function (todo) {
         todo.title = oldTitle;
@@ -47,8 +47,8 @@ todo.controller('todoCtrl', function TodoCtrl($scope, todoStorage) {
         todo.date = oldDate;
     };
 
-	$scope.removeTodo = function (todo) {
-		todos.splice(todos.indexOf(todo), 1);
-	};
+    $scope.removeTodo = function (todo) {
+        todos.splice(todos.indexOf(todo), 1);
+    };
 
 });
