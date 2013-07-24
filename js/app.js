c017aca91b515f5ea08dc247e749e0289a33d141
Max <maksr51314@gmail.com>
2013-07-24 14:08:46 +0300

    delete ui datapicker

--- js/app.js
+++ js/app.js
@@ -37,21 +37,3 @@ todo.directive('asidemy', function(){
         templateUrl:'partials/content.html'
     }
 });
-
-todo.directive('datepicker', function() {
-    return {
-        restrict: 'A',
-        require : 'ngModel',
-        link : function (scope, element, attrs, ngModelCtrl) {
-            $(function(){
-                element.datepicker({
-                    dateFormat:'dd/mm/yy',
-                    onSelect:function (date) {
-                        ngModelCtrl.$setViewValue(date);
-                        scope.$apply();
-                    }
-                });
-            });
-        }
-    }
-});
