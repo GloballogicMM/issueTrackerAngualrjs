f4b402f2904f8d24830551673f71004ef9db2628
Max <maksr51314@gmail.com>
2013-07-25 16:14:46 +0300

    factory users

---
+++ js/services/status.js
@@ -0,0 +1,8 @@
+todo.factory('factories', function () {
+    var factories = [
+        {name: 'Ivan Ivanov', number: 0},
+        {name: 'Sergei Sergeev', number: 1},
+        {name: 'Petr Petrovich', number: 2}
+    ];
+    return users;
+});
