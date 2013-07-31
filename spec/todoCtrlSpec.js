c648469b4f5e0713752068c9b646dd83b6799dcb
Max <maksr51314@gmail.com>
2013-07-31 09:32:14 +0300

    set default settings for tests

--- spec/todoCtrlSpec.js
+++ spec/todoCtrlSpec.js
@@ -1,6 +1,19 @@
 describe("testting Todo controller", function(){
+    //module and inject - in angular-mocks.js
     //load module before load
     beforeEach(module("todo"));
 
-    beforeEach()
+//create global scope
+    var scope;
+    //do inject controller
+    beforeEach(inject(function($rootScope, $controller){
+        //create default scope
+        scope = $rootScope.$new();
+        //use default scope in controller
+        $controller("todoCtrl", {
+            $scope: scope
+        });
+    }));
+
+    describe("")
 });
